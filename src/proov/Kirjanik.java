f5728cbbc345229cf42519009fcbadeb861fbb6d
unknown <Evelin.Jogi@R2320.hm.ee>
2016-02-05 14:36:23 +0200

    Lahendus.

---
+++ src/proov/Kirjanik.java
@@ -0,0 +1,14 @@
+package proov;
+
+/**
+ * Created by Evelin.Jogi on 3.02.2016.
+ */
+public class Kirjanik {
+    public static void main(String[] args) {
+        int tindiKogus = 60;
+        Pastakas pastakas = new Pastakas(tindiKogus);
+        pastakas.kirjuta("Elu oleks palju lihtsam, kui meil oleks selle lähtekood.");
+        pastakas.prindiPaljuTintiAlles();
+        pastakas.kirjuta("Õiged progejad ei kommenteeri oma koodi. Kui seda oli raske kirjutada, siis peab olema seda ka raske lugeda!");
+    }
+}
